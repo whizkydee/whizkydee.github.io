e2c12d8b7b5bf834122a6ad0ae9d05ea5429abf5
Olaolu Olawuyi <whiskydee@gmail.com>
2018-04-10 04:25:57 +0100

    update data store

--- src/js/data.js
+++ src/js/data.js
@@ -12,8 +12,8 @@ const data = {
     <h1>about me</h1>
     <div>lover of - unicorns · purples · donuts</div>
     <p>I'm Olaolu Olawuyi, a 17-year old, and a student of the University of Lagos, Nigeria. I live a simple life, I love being optimistic about everything and I smile a lot. I play piano or drums when I'm graced with the chance. I'm an Impostor! And yeah, I'm a July born - July 01.</p>
-    <p>As a web developer, I've had 7+ years of experience building websites and applications ranging from simple to very complex ones. I focus on creating blazing fast and minimalistic user-friendly experiences. I've worked with organizations like Pneuma media, MrShopperHQ, Teens Can Code Africa and a bunch in the past to deliver solutions at various points.</p>
-    <p>I love to build applications in core <b>JavaScript</b> as it's a very powerful language. I use frameworks like <b>React.js</b>, <b>Vue.js</b> and a bunch.</p>
+    <p>As a web developer, I've had 7+ years of experience building websites and applications ranging from simple to very complex ones. I focus on creating blazing fast and very minimalistic user-friendly experiences. I've worked with organizations like Pneuma media, MrShopperHQ, Teens Can Code Africa and a bunch in the past to deliver services and solutions.</p>
+    <p>I love to build applications in core <b>JavaScript</b> as it's a very powerful language. I use frameworks like <b>React.js</b>, <b>Vue.js</b>, <b>Node.js</b> and a bunch.</p>
     `,
     name: 'about'
   },
@@ -22,10 +22,12 @@ const data = {
     <h1>contact</h1>
     <form action="https://formspree.io/mrolaolu@gmail.com"
       method="POST">
-      <input type="text" name="name" id="name" placeholder="Your Name">
-      <input type="email" name="_replyto" id="email" placeholder="Email Address">
-      <textarea id="contact_message" placeholder="Your Message" name="message" rows="7"></textarea>
+      <input type="hidden" name="_subject" value="Message from olaolu.me!">
+      <input type="text" name="name" placeholder="Your Name" required minlength="3">
+      <input type="email" name="_replyto" placeholder="Email Address" required minlength="5">
+      <textarea placeholder="Your Message" name="message" rows="7" required minlength="30"></textarea>
       <input type="submit" value="shoot!" id="submit">
+      <span>or <a href="https://github.com/whizkydee/ama" target="_blank" rel="noopener">Ask me anything</a> instead.</span>
     </form>
     `,
     name: 'contact'
