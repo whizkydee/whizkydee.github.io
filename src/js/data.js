357e8a410a58a32aca1b677960b3ffad428d2935
Olaolu Olawuyi <whiskydee@gmail.com>
2018-04-22 19:53:35 +0100

    update bio

--- src/js/data.js
+++ src/js/data.js
@@ -2,7 +2,7 @@ const data = {
   home: {
     content: `
     <h1>Hi. I'm Olaolu,</h1>
-    <p>A 17 year old creative front end developer and designer in <a href="https://www.google.com/maps/place/Lagos/@6.5483768,3.1438711,11z/data=!3m1!4b1!4m5!3m4!1s0x103b8b2ae68280c1:0xdc9e87a367c3d9cb!8m2!3d6.5243793!4d3.3792057" target="_blank">Lagos, Nigeria</a>. I'm interested in the web, open source, artificial intelligence, tech, design and music. You can check out my work on <a href="https://github.com/whizkydee" rel="noopener" target="_blank">GitHub</a>.</p>
+    <p>A 17 year old creative front end developer and designer in <a href="https://www.google.com/maps/place/Lagos/@6.5483768,3.1438711,11z/data=!3m1!4b1!4m5!3m4!1s0x103b8b2ae68280c1:0xdc9e87a367c3d9cb!8m2!3d6.5243793!4d3.3792057" target="_blank">Lagos, Nigeria</a>. I'm interested in the web, open source, artificial intelligence, photography, design and music. You can check out my work on <a href="https://github.com/whizkydee" rel="noopener" target="_blank">GitHub</a>.</p>
     <a href="/#about" onclick="aboutLink.click()">about</a>
     `,
     name: 'home'
@@ -11,7 +11,7 @@ const data = {
     content: `
     <h1>about me</h1>
     <div>lover of - unicorns · purples · donuts</div>
-    <p>I'm Olaolu Olawuyi, a 17 year old, and a student of the University of Lagos, Nigeria. I live a simple life, I love being optimistic about things and I smile a lot. I play piano or drums when I'm graced with the chance. I'm an impostor, and yeah, I'm a July born - July 01.</p>
+    <p>I'm Olaolu Olawuyi, a 17 year old, and a student of the University of Lagos, Nigeria. I live a simple life, I love being optimistic about things and I smile a lot. I explore my funtography skils when I'm not busy or in front of my laptop. I'm an impostor, and yeah, I'm a July born - July 01.</p>
     <p>As a web developer, I've had 7+ years of experience building websites and applications ranging from simple to very complex ones. I focus on creating blazing fast and very elegant user experiences. I head tech operations at <a href="http://teenscancode.com.ng" rel="noopener" target="_blank">Teens Can Code Africa</a>, a non-profit organization helping to expand teens' access to technology through problem-solving and coding in Africa. I've worked with organizations like Pneuma media, MrShopperHQ and a bunch in the past to deliver services and solutions. I'm also the creator of the famous <a href="https://marketplace.visualstudio.com/items?itemName=whizkydee.material-palenight-theme" rel="noopener" target="_blank">Palenight theme</a> for VS Code which currently has 100K+ installs.</p>
     <p>I love to build applications in core <b>JavaScript</b>. I use frameworks like React, Vue, Node.js and a bunch. I'm currently open to ideas and very flexible remote or contract work.</p>
     <p>Love my work? A box of unicorn donuts would be nice 😊. Please <a href="https://bit.ly/payOlaolu" target="_blank" rel="noopener">donate</a>.</p>
