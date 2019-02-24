dff2f50b6f1bfb33663139b31501c419ad88077c
Olaolu Olawuyi <mrolaolu@gmail.com>
2019-02-24 12:53:16 +0100

    update bio

--- src/js/data.js
+++ src/js/data.js
@@ -2,7 +2,7 @@
 const data = {
   home: {
     content: `
-    <h1>Hi<span>!</span> I'm Olaolu<span>,</span></h1>
+    <h1>Hi<span>.</span> I'm Olaolu<span>,</span></h1>
     <p>A creative front end developer and designer in <a href="https://www.google.com/maps/place/Lagos/@6.5483768,3.1438711,11z/data=!3m1!4b1!4m5!3m4!1s0x103b8b2ae68280c1:0xdc9e87a367c3d9cb!8m2!3d6.5243793!4d3.3792057" target="_blank" rel="noopener">Lagos, Nigeria</a>. I'm interested in the web, open source, artificial intelligence, tech, design and music. You can check out my work on <a href="https://github.com/whizkydee" target="_blank" rel="noopener">GitHub</a>.</p>
     <a href="/#about" onclick="aboutLink.click()">about</a>
     `,
@@ -12,9 +12,9 @@ const data = {
     content: `
     <h1>about me</h1>
     <p>lover of - unicorns · purples · donuts</p>
-    <p>I'm a creative currently based in Lagos, Nigeria; living a simple life being optimistic about things and smiling most of the time. When I'm not in front of my computer, I like to explore (what I call) "funtography" skills.</p>
-    <p>Over the past 8 years, I've built products for businesses globally ranging from basic websites to very complex applications with focus on creating blazing fast and elegant user experiences. I previously led the front-end engineering team at Conectar LLC where we're building one of the biggest React applications in the world. I deliver quality stuff while doing freelance and consultation for a bunch of startups and organizations around the world. I also created the <a href="https://marketplace.visualstudio.com/items?itemName=whizkydee.material-palenight-theme" rel="noopener" target="_blank">Palenight theme</a> for VS Code with hundreds of thousands of downloads.</p>
-    <p>I'm passionate about web performance and JavaScript applications. Open for consultation, ideas and remote or contract work. Say Hi!</p>
+    <p>I like to call myself a User Experience Strategist and front end developer. I'm currently based in the city of Lagos in Nigeria... living a simple life being optimistic about things and smiling a lot. When I'm not in front of my computer, I like to just relax and listen to some music or play with my iPhone camera.</p>
+    <p>Over the past 8 years, I've built products for businesses globally ranging from basic websites to very complex applications with focus on creating blazing fast, elegant and accessible user experiences. I previously led the front-end engineering team at Conectar LLC where we're building one of the biggest React applications in the world. I deliver quality stuff while doing freelance and consulting for a bunch of organizations around the world. I also created the popular <a href="https://marketplace.visualstudio.com/items?itemName=whizkydee.material-palenight-theme" rel="noopener" target="_blank">Palenight theme</a> for VS Code with hundreds of thousands of downloads.</p>
+    <p>I'm passionate about web performance, great user experiences and JavaScript applications, also open for consultation, and remote or contract work. Leave a message!</p>
     `,
     tabId: 'about'
   },
