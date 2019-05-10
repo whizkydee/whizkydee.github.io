7da9fff05fa4c632514849c036dd95b77332ed60
Olaolu Olawuyi <mrolaolu@gmail.com>
2019-05-10 10:57:49 +0100

    Update site content

--- dest/js/script.js
+++ dest/js/script.js
@@ -1,23 +1,25 @@
 'use strict';
 
 // INFO: see https://olaolu.me/src/js/script.js for the real sauce.
-var body = document.body,
-    docElem = document.documentElement;
+var body = document.body;
+var docElem = document.documentElement;
+
 var routeTo = function routeTo(target) {
   var title = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : target.getAttribute('data-navigator');
 
-  var tabTitle = title,
-      currentTab = data[tabTitle],
-      main = document.querySelector('main');
+  var tabTitle = title;
+  var currentTab = data[tabTitle];
+  var main = document.querySelector('main');
 
   docElem.style.setProperty('background-color', 'var(--' + tabTitle + '-border)');
   document.title = title.charAt(0).toUpperCase() + title.slice(1) + ' \xB7 Olaolu Olawuyi';
   body.style.setProperty('background-color', 'var(--' + tabTitle + '-bg)');
 
-  body.className = body.className !== currentTab['tabId'] ? currentTab['tabId'] : body.className;
-  main.className = main.className !== currentTab['tabId'] ? currentTab['tabId'] : main.className;
+  body.className = body.className !== currentTab.tabId ? currentTab.tabId : body.className;
+  main.className = main.className !== currentTab.tabId ? currentTab.tabId : main.className;
 
   main.innerHTML = main.innerHTML !== currentTab.content ? currentTab.content : main.innerHTML;
+
   document.querySelector('meta[name="theme-color"]').setAttribute('content', getComputedStyle(docElem).getPropertyValue('--' + tabTitle + '-bg'));
 
   if (!target.classList.contains('active')) {
@@ -29,9 +31,9 @@ var routeTo = function routeTo(target) {
 };
 
 var navigators = [].slice.call(document.querySelectorAll('[data-navigator]'));
-var hash = location.hash.replace('#', ''),
-    pages = ['home', 'about', 'contact'],
-    aboutLink = navigators[1];
+var hash = location.hash.replace('#', '');
+var pages = ['home', 'about', 'contact'];
+var aboutLink = navigators[1];
 
 if (!pages.includes(hash) || !hash) navigators[0].className = 'active';
 
@@ -53,10 +55,10 @@ window.addEventListener('hashchange', function () {
 });
 
 var positionSocialIcons = function positionSocialIcons() {
-  var elemsHeight = document.querySelector('header').clientHeight + document.querySelector('main').clientHeight,
-      bodyHeight = document.body.clientHeight,
-      availHeight = bodyHeight - elemsHeight,
-      social = document.querySelector('aside');
+  var elemsHeight = document.querySelector('header').clientHeight + document.querySelector('main').clientHeight;
+  var bodyHeight = document.body.clientHeight;
+  var availHeight = bodyHeight - elemsHeight;
+  var social = document.querySelector('aside');
 
   if (bodyHeight > elemsHeight) {
     social.style.marginTop = window.matchMedia('(orientation: portrait)').matches ? availHeight - 130 + 'px' : availHeight - 70 + 'px';
--- src/js/data.js
+++ src/js/data.js
@@ -3,20 +3,21 @@ const data = {
   home: {
     content: `
     <h1>Hi<span>.</span> I'm Olaolu<span>,</span></h1>
-    <p>A creative front end developer and designer in <a href="https://www.google.com/maps/place/Lagos/@6.5483768,3.1438711,11z/data=!3m1!4b1!4m5!3m4!1s0x103b8b2ae68280c1:0xdc9e87a367c3d9cb!8m2!3d6.5243793!4d3.3792057" target="_blank" rel="noopener">Lagos, Nigeria</a>. I'm interested in the web, open source, artificial intelligence, tech, design and music. You can check out my work on <a href="https://github.com/whizkydee" target="_blank" rel="noopener">GitHub</a>.</p>
+    <p>A Front end engineer and UX Strategist based in <a href="https://www.google.com/maps/place/Lagos/@6.5483768,3.1438711,11z/data=!3m1!4b1!4m5!3m4!1s0x103b8b2ae68280c1:0xdc9e87a367c3d9cb!8m2!3d6.5243793!4d3.3792057" target="_blank" rel="noopener">Lagos, Nigeria</a>.I'm passionate about web development best practices, crafting design systems (which I've done for almost every project), interface engineering, performance, user experience and overall product success. You can check out some of my work on <a href="https://github.com/whizkydee" target="_blank" rel="noopener">GitHub</a>.</p>
     <a href="/#about" onclick="aboutLink.click()">about</a>
     `,
-    tabId: 'home'
+    tabId: 'home',
   },
   about: {
     content: `
     <h1>about me</h1>
     <p>lover of - unicorns · purples · donuts</p>
-    <p>I like to call myself a User Experience Strategist and front end developer. I'm currently based in the city of Lagos in Nigeria... living a simple life being optimistic about things and smiling a lot. When I'm not in front of my computer, I like to just relax and listen to some music or play with my iPhone camera.</p>
-    <p>Over the past 8 years, I've built products for businesses globally ranging from basic websites to very complex applications with focus on creating blazing fast, elegant and accessible user experiences. I previously led the front-end engineering team at Conectar LLC where we're building one of the biggest React applications in the world. I deliver quality stuff while doing freelance and consulting for a bunch of organizations around the world. I also created the popular <a href="https://marketplace.visualstudio.com/items?itemName=whizkydee.material-palenight-theme" rel="noopener" target="_blank">Palenight theme</a> for VS Code with hundreds of thousands of downloads.</p>
+    <p>I like to call myself a UX Strategist and front end developer. I'm currently based in the city of Lagos in Nigeria... living a simple life being optimistic about things and smiling a lot. When I'm not in front of my computer, I'm either trying to make music or fondling with my camera and trying new things.</p>
+    <p>Over the past 8 years, I've built products for businesses all around the globe ranging from basic websites to complex solutions with focus on creating blazing fast, elegant and accessible user experiences. Right now, I'm a Senior Front End Engineering contractor with <a href="https://pixel2html.com" rel="noopener noreferrer" target="_blank">Pixel2HTML</a>, an agency comprised of some of the most talented front end developers scattered around the globe. Previously, I led the front-end engineering team at Conectar LLC through building several React applications into a single powerful online learning platform. I deliver quality stuff freelancing and consulting for a bunch of organizations internationally. I also created the <a href="https://marketplace.visualstudio.com/items?itemName=whizkydee.material-palenight-theme" rel="noopener" target="_blank">Palenight theme</a> for VS Code with hundreds of thousands of downloads.</p>
     <p>I'm passionate about web performance, great user experiences and JavaScript applications, also open for consultation, and remote or contract work. Leave a message!</p>
+    <p>(Available for full time roles starting July 2019)</p>
     `,
-    tabId: 'about'
+    tabId: 'about',
   },
   contact: {
     content: `
@@ -32,6 +33,6 @@ const data = {
       <span>or <a href="https://github.com/whizkydee/ama" target="_blank" rel="noopener">ask me anything</a> instead.</span>
     </form>
     `,
-    tabId: 'contact'
-  }
-};
+    tabId: 'contact',
+  },
+}
